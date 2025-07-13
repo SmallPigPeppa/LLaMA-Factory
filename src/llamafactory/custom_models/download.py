6bec052534a44862ecfbaff219695d7324046aaa
lwz <905328134@qq.com>
2025-07-13 23:52:02 +0800

    initial

--- src/llamafactory/custom_models/download.py
+++ src/llamafactory/custom_models/download.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 
 # 填入你实际用的 model_id，比如 "Qwen/Qwen2.5-VL-7B-Instruct"
-local_dir = "~/qwen2_5_vl_my"
+local_dir = "/home/tiger/qwen2_5_vl_my"
 snapshot_download(
     repo_id="Qwen/Qwen2.5-VL-7B-Instruct",
     local_dir=local_dir,
