f91cd1f0a9de45fd07f3a16095e2a015d077472b
lwz <905328134@qq.com>
2025-07-14 18:10:03 +0800

    initial

--- custom_models/download.py
+++ custom_models/download.py
@@ -1,9 +1,16 @@
 from huggingface_hub import snapshot_download
 
-# 填入你实际用的 model_id，比如 "Qwen/Qwen2.5-VL-7B-Instruct"
-local_dir = "/home/tiger/qwen2_5_vl_my"
+# local_dir = "/home/tiger/qwen2_5_vl_7b"
+# snapshot_download(
+#     repo_id="Qwen/Qwen2.5-VL-7B-Instruct",
+#     local_dir=local_dir,
+#     # 如果你需要访问私有仓库，这里可以加 token：
+#     # use_auth_token="YOUR_HF_TOKEN",
+# )
+
+local_dir = "/home/tiger/qwen2_5_vl_3b"
 snapshot_download(
-    repo_id="Qwen/Qwen2.5-VL-7B-Instruct",
+    repo_id="Qwen/Qwen2.5-VL-3B-Instruct",
     local_dir=local_dir,
     # 如果你需要访问私有仓库，这里可以加 token：
     # use_auth_token="YOUR_HF_TOKEN",
