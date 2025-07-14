e4f64fe8b0b5cea05562d1230fed0ce404f20cd2
lwz <905328134@qq.com>
2025-07-14 18:06:34 +0800

    initial

--- sft_qwen2_5_vl.sh
+++
@@ -1,2 +0,0 @@
-export WANDB_PROJECT="llama-factory"
-FORCE_TORCHRUN=1 llamafactory-cli train examples/train_full/qwen2_5vl_full_sft.yaml
---
+++ sft_qwen2_5_vl_7b.sh
@@ -0,0 +1,2 @@
+export WANDB_PROJECT="llama-factory"
+FORCE_TORCHRUN=1 llamafactory-cli train examples/train_full/qwen2_5vl_7b_full_sft.yaml
