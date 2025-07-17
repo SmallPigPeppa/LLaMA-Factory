01c9ec40f99efd0c7ec01cd97732939206f4a85d
lwz <905328134@qq.com>
2025-07-17 22:14:36 +0800

    initial

--- custom_models/qwen2_5_vl_3b_mspe/modeling_qwen2_5_vl_mspe.py
+++ custom_models/qwen2_5_vl_3b_mspe/modeling_qwen2_5_vl_mspe.py
@@ -1696,7 +1696,7 @@ class Qwen2_5_VLModel(Qwen2_5_VLPreTrainedModel):
 
             if attention_mask is not None:
                 attention_mask = attention_mask.to(inputs_embeds.device)
-
+        import pdb;pdb.set_trace()
         # if we get 4D attention mask we cannot calculate rope deltas anymore. TODO @raushan fixme
         if position_ids is None and (attention_mask is None or attention_mask.ndim == 2):
             # calculate RoPE index once per generation in the pre-fill stage only
