76e98e624f5e4a1cb8778f96157627093b732876
lwz <905328134@qq.com>
2025-07-28 14:57:56 +0800

    initial

--- custom_models/qwen2_5_vl_3b_mspe/a_forward.py
+++ custom_models/qwen2_5_vl_3b_mspe/a_forward.py
@@ -30,7 +30,8 @@ def recompose_windows(window_adp_ps, hidden_states_ps, position_embeddings_ps, w
         ))
         # window_index_list.append(window_index_ps[ps][start:end])
         # unified to ps=7
-        window_index_list.append(window_index_ps[ps][start:end] * ((ps / 7) ** 2))
+        scale = torch.tensor((ps / 7) ** 2, dtype=window_index_ps[ps].dtype, device=window_index_ps[ps].device)
+        window_index_list.append(window_index_ps[ps][start:end] * scale)
 
         # 更新cursor
         window_cursor_ps[ps] += 1
