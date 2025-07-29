f93370dc6d4c926b493aa0bcb6889a4a50f2c46e
lwz <905328134@qq.com>
2025-07-29 16:03:24 +0800

    initial

--- custom_models/qwen2_5_vl_3b_mspe/a_forward.py
+++ custom_models/qwen2_5_vl_3b_mspe/a_forward.py
@@ -27,7 +27,7 @@ def recompose_windows(window_adp_ps, hidden_states_ps, position_embeddings_ps, w
         # unified to ps=7
         scale = torch.tensor((ps / 7) ** 2, dtype=window_index_ps[ps].dtype, device=window_index_ps[ps].device)
         # window_index_list.append(window_index_ps[ps][start:end] * scale)
-        import pdb; pdb.set_trace()
+
         window_index_list.append(window_index_ps[ps][start:end])
 
         # 更新累计长度
@@ -37,6 +37,7 @@ def recompose_windows(window_adp_ps, hidden_states_ps, position_embeddings_ps, w
     hidden_states = torch.cat(hidden_states_list, dim=0)
     position_embeddings_cos = torch.cat([emb[0] for emb in position_embeddings_list], dim=0)
     position_embeddings_sin = torch.cat([emb[1] for emb in position_embeddings_list], dim=0)
+    import pdb;pdb.set_trace()
     window_index = torch.cat(window_index_list, dim=0)
     cu_window_seqlens = torch.tensor(cu_window_seqlens, dtype=cu_window_seqlens[-1].dtype,
                                      device=cu_window_seqlens[-1].device)
