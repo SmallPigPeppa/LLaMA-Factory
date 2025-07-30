c6cc5be836f2473253278ce5390370a6a787fcf3
lwz <905328134@qq.com>
2025-07-30 16:06:43 +0800

    initial

--- custom_models/qwen2_5_vl_3b_mspe/a_forward.py
+++ custom_models/qwen2_5_vl_3b_mspe/a_forward.py
@@ -4,7 +4,7 @@ from custom_models.qwen2_5_vl_3b_mspe.debug_utilsv2 import repatchify
 import torch.nn.functional as F
 
 
-def recompose_windows(window_adp_ps, hidden_states_ps, position_embeddings_ps, window_index_ps, cu_window_seqlens_ps,spatial_merge_unit):
+def recompose_windows(window_adp_ps, hidden_states_ps, position_embeddings_ps, window_index_ps, cu_window_seqlens_ps, spatial_merge_unit,grid_thw_ps):
     hidden_states_list = []
     position_embeddings_list = []
     window_index_list = []
@@ -25,10 +25,26 @@ def recompose_windows(window_adp_ps, hidden_states_ps, position_embeddings_ps, w
             position_embeddings_ps[ps][0][start:end],
             position_embeddings_ps[ps][1][start:end]
         ))
-        # window_index_list.append(window_index_ps[ps][start:end])
         # unified to min patchsize
-        scale = torch.tensor((ps / min_ps) ** 2, dtype=window_index_ps[ps].dtype, device=window_index_ps[ps].device)
-        window_index_list.append(window_index_ps[ps][win_start:win_end] * scale)
+        # scale = torch.tensor((ps / min_ps) ** 2, dtype=window_index_ps[ps].dtype, device=window_index_ps[ps].device)
+        # window_index_list.append(window_index_ps[ps][win_start:win_end] * scale)
+
+        f = ps // min_ps
+
+        # 原/新网格尺寸
+        T, H, W = grid_thw_ps[ps]
+        _, H0, W0 = grid_thw_ps[min_ps]
+
+        # 原始扁平索引
+        inds = window_index_ps[ps][win_start:win_end]
+
+        # 一步拆 t,h,w，再一步合 new_inds
+        t, h, w = torch.unravel_index(inds, (T, H, W))
+        new_inds = torch.ravel_multi_index((t, h * f, w * f), (T, H0, W0))
+
+        window_index_list.append(new_inds)
+
+
 
         # 更新累计长度
         cu_window_seqlens.append(cu_window_seqlens[-1] + (end - start))
