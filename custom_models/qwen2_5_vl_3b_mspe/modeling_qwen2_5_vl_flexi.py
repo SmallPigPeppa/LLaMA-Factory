495ccc8b21338e94d7d9f3ccc2368ca6da1a670d
lwz <905328134@qq.com>
2025-07-31 22:03:06 +0800

    initial

--- custom_models/qwen2_5_vl_3b_mspe/modeling_qwen2_5_vl_flexi.py
+++ custom_models/qwen2_5_vl_3b_mspe/modeling_qwen2_5_vl_flexi.py
@@ -692,11 +692,11 @@ class Qwen2_5_VisionTransformerPretrainedModel(Qwen2_5_VLPreTrainedModel):
             `torch.Tensor`: hidden_states.
         """
 
-        patch_sizes = [14, 28]
+        patch_sizes = [7, 14, 28]
         # patch_sizes = [28]
         # patch_sizes = [14]
         # patch_sizes = [7]
-        # patch_sizes = [7, 14]
+        patch_sizes = [7, 28]
         grid_thw_ps = {}
         hidden_states_ps = {}
         rotary_pos_emb_ps = {}
