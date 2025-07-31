8c5b19a0ab7fd5c81d3ae734bd74579c47ef5486
lwz <905328134@qq.com>
2025-07-31 18:44:12 +0800

    initial

--- src/llamafactory/model/__init__.py
+++ src/llamafactory/model/__init__.py
@@ -27,7 +27,3 @@ __all__ = [
     "load_valuehead_params",
 ]
 
-import pdb;pdb.set_trace()
-from transformers import AutoImageProcessor
-from custom_models.qwen2_5_vl_3b_mspe.A_image_processor import Qwen2VLImageProcessor
-AutoImageProcessor.register("Qwen2VLImageProcessorMSPE", Qwen2VLImageProcessor)
--- src/train.py
+++ src/train.py
@@ -13,6 +13,10 @@
 # limitations under the License.
 
 from llamafactory.train.tuner import run_exp
+import pdb;pdb.set_trace()
+from transformers import AutoImageProcessor
+from custom_models.qwen2_5_vl_3b_mspe.A_image_processor import Qwen2VLImageProcessor
+AutoImageProcessor.register("Qwen2VLImageProcessorMSPE", Qwen2VLImageProcessor)
 
 
 def main():
