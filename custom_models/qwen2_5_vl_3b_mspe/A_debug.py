578e05ffdaf1dacbd73c27637b3d9a671e332845
lwz <905328134@qq.com>
2025-07-31 20:33:20 +0800

    initial

--- custom_models/qwen2_5_vl_3b_mspe/A_debug.py
+++ custom_models/qwen2_5_vl_3b_mspe/A_debug.py
@@ -1,7 +1,8 @@
 from transformers import AutoImageProcessor
 
 from custom_models.qwen2_5_vl_3b_mspe.A_image_processor import Qwen2VLImageProcessor
-AutoImageProcessor.register("Qwen2VLImageProcessorMSPE", Qwen2VLImageProcessor)
+from custom_models.qwen2_5_vl_3b_mspe.configuration_qwen2_5_vl import Qwen2_5_VLVisionConfig
+AutoImageProcessor.register(config_class=Qwen2_5_VLVisionConfig, image_processor_class=Qwen2VLImageProcessor)
 
 
 processor = AutoImageProcessor.from_pretrained("custom_models/qwen2_5_vl_3b_mspe")
