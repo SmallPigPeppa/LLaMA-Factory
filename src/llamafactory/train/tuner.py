85993de0121981c7ec72a90c8add84b328f87db3
lwz <905328134@qq.com>
2025-07-31 18:47:44 +0800

    initial

--- src/llamafactory/train/tuner.py
+++ src/llamafactory/train/tuner.py
@@ -15,7 +15,8 @@
 from transformers import AutoImageProcessor
 from custom_models.qwen2_5_vl_3b_mspe.A_image_processor import Qwen2VLImageProcessor
 AutoImageProcessor.register("Qwen2VLImageProcessorMSPE", Qwen2VLImageProcessor)
-print("注册表：", AutoImageProcessor._mapping)
+print(AutoImageProcessor.__dict__)
+
 
 
 import os
