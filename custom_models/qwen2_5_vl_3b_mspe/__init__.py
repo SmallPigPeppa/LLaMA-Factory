cd44e6997f9b38acf00726363d4fa01f74d79c97
lwz <905328134@qq.com>
2025-07-31 18:32:52 +0800

    initial

--- custom_models/qwen2_5_vl_3b_mspe/__init__.py
+++ custom_models/qwen2_5_vl_3b_mspe/__init__.py
@@ -11,6 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from transformers import AutoImageProcessor
+from .A_image_processor import Qwen2VLImageProcessor
+AutoImageProcessor.register("Qwen2VLImageProcessorMSPE", Qwen2VLImageProcessor)
+
 from typing import TYPE_CHECKING
 
 from transformers.utils import _LazyModule
@@ -27,6 +31,4 @@ else:
     _file = globals()["__file__"]
     sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
 
-from transformers import AutoImageProcessor
-from .A_image_processor import Qwen2VLImageProcessor
-AutoImageProcessor.register("Qwen2VLImageProcessorMSPE", Qwen2VLImageProcessor)
+
