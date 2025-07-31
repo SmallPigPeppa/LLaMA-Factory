8ceea58899e5df590c70732d4cd15baf32f49440
lwz <905328134@qq.com>
2025-07-31 20:40:37 +0800

    initial

--- src/llamafactory/train/tuner.py
+++ src/llamafactory/train/tuner.py
@@ -13,6 +13,12 @@
 # limitations under the License.
 # import pdb;pdb.set_trace()
 
+# register for MSPE
+from transformers import AutoImageProcessor
+from custom_models.qwen2_5_vl_3b_mspe.A_image_processor import Qwen2VLImageProcessorMSPE
+from custom_models.qwen2_5_vl_3b_mspe.configuration_qwen2_5_vl import Qwen2_5_VLVisionConfig
+AutoImageProcessor.register(config_class=Qwen2_5_VLVisionConfig, image_processor_class=Qwen2VLImageProcessorMSPE)
+
 import os
 import shutil
 from typing import TYPE_CHECKING, Any, Optional
