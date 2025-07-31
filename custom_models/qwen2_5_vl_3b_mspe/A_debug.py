031a3dfc5056fe3e95ad6de3363c90d057774da7
lwz <905328134@qq.com>
2025-07-31 20:28:26 +0800

    initial

--- custom_models/qwen2_5_vl_3b_mspe/A_debug.py
+++ custom_models/qwen2_5_vl_3b_mspe/A_debug.py
@@ -1,13 +1,7 @@
-from transformers import AutoConfig, AutoImageProcessor
-import os
+from transformers import AutoImageProcessor
 
-# 1. 注册自定义 ImageProcessor
 from custom_models.qwen2_5_vl_3b_mspe.A_image_processor import Qwen2VLImageProcessor
-AutoConfig.register("Qwen2VLImageProcessorMSPE", Qwen2VLImageProcessor)
+AutoImageProcessor.register("Qwen2VLImageProcessorMSPE", Qwen2VLImageProcessor)
 
 
-# 3. 用 from_pretrained 自动加载
 processor = AutoImageProcessor.from_pretrained("custom_models/qwen2_5_vl_3b_mspe")
-
-print("加载的 processor 实例：", processor)
-print("类型：", type(processor))
