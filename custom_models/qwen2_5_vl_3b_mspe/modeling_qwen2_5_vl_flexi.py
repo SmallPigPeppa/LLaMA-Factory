fde47d3dc6c0e29e9284ad6687781eeb43e5df77
lwz <905328134@qq.com>
2025-08-05 14:50:25 +0800

    initial

--- custom_models/qwen2_5_vl_3b_mspe/modeling_qwen2_5_vl_flexi.py
+++ custom_models/qwen2_5_vl_3b_mspe/modeling_qwen2_5_vl_flexi.py
@@ -694,10 +694,6 @@ class Qwen2_5_VisionTransformerPretrainedModel(Qwen2_5_VLPreTrainedModel):
         """
 
         patch_sizes = [7, 14, 28]
-        # patch_sizes = [28]
-        # patch_sizes = [14]
-        # patch_sizes = [7]
-        # patch_sizes = [7, 28]
         grid_thw_ps = {}
         hidden_states_ps = {}
         rotary_pos_emb_ps = {}
