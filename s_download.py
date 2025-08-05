d49615bb26ef0e44773bb4df1bf198a9e59df8df
lwz <905328134@qq.com>
2025-08-05 23:06:41 +0800

    initial

--- s_download.py
+++ s_download.py
@@ -1,15 +1,9 @@
-import os
 from huggingface_hub import snapshot_download
-
-os.environ["HF_HOME"] = "/mnt/bn/liuwenzhuo-hl-data/hf_cache"
-os.environ["HF_DATASETS_CACHE"] = "/mnt/bn/liuwenzhuo-hl-data/hf_cache/datasets"
-os.environ["HF_METRICS_CACHE"] = "/mnt/bn/liuwenzhuo-hl-data/hf_cache/metrics"
-os.environ["TRANSFORMERS_CACHE"] = "/mnt/bn/liuwenzhuo-hl-data/hf_cache/transformers"
 
 snapshot_download(
     repo_id="lmms-lab/LLaVA-NeXT-Data",
     repo_type="dataset",
-    local_dir="/mnt/bn/liuwenzhuo-hl-data/hf_cache/LLaVA-NeXT-Data",
+    cache_dir="/mnt/bn/liuwenzhuo-hl-data/hf_cache",
     max_workers=64,              # 线程数可调
     resume_download=True,
 )
