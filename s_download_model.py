b8e322463c5775f15f65118034070275352f0235
lwz <905328134@qq.com>
2025-08-06 00:36:01 +0800

    initial

--- s_download_model.py
+++ s_download_model.py
@@ -4,6 +4,6 @@ snapshot_download(
     repo_id="Qwen/Qwen2.5-VL-3B-Instruct",
     repo_type="model",
     local_dir="/mnt/bn/liuwenzhuo-hl-data/Qwen/Qwen2.5-VL-3B-Instruct",
-    max_workers=32,
+    max_workers=16,
     resume_download=True,
 )
