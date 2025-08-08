84a082824276256a493376c144786b5060d4920d
lwz <905328134@qq.com>
2025-08-08 17:07:07 +0800

    initial

--- s_download_data.py
+++ s_download_data.py
@@ -4,7 +4,7 @@ from huggingface_hub import snapshot_download
 snapshot_download(
     repo_id="lmms-lab/LLaVA-NeXT-Data",
     repo_type="dataset",
-    cache_dir="/home/tiger/hf_cache",
-    max_workers=64,              # 线程数可调
+    cache_dir="/home/tiger/hf_cache_new",
+    max_workers=128,              # 线程数可调
     resume_download=True,
 )
