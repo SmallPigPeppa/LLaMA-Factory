37d08a6a08d977c41daed0554d56ed7c0209e2b8
lwz <905328134@qq.com>
2025-08-11 16:17:13 +0800

    initial

--- src/llamafactory/data/loader.py
+++ src/llamafactory/data/loader.py
@@ -145,7 +145,8 @@ def _load_single_dataset(
         dataset = load_dataset(
             "parquet",
             # data_files="/mnt/bn/liuwenzhuo-hl-data/779k/data_renamed/*.parquet",
-            data_files="/mnt/bn/liuwenzhuo-hl-data/hf_cache/hub/datasets--lmms-lab--LLaVA-NeXT-Data/snapshots/c8aef391ce214167c4ebc7f06bc05a50dee2e75f/data/*.parquet",
+            # data_files="/mnt/bn/liuwenzhuo-hl-data/hf_cache/hub/datasets--lmms-lab--LLaVA-NeXT-Data/snapshots/c8aef391ce214167c4ebc7f06bc05a50dee2e75f/data/*.parquet",
+            data_files='/mnt/bn/liuwenzhuo-hl-data/779k-shuffle/*.parquet',
             split="train",
             num_proc=num_proc,
             streaming=data_args.streaming and dataset_attr.load_from != "file"
