054207251c0ecdf95a78da7406c4fc3bcad31d43
lwz <905328134@qq.com>
2025-08-12 22:44:26 +0800

    initial

--- custom_models/qwen2_5_vl_3b_mspe/A_image_processor.py
+++ custom_models/qwen2_5_vl_3b_mspe/A_image_processor.py
@@ -288,7 +288,7 @@ class Qwen2VLImageProcessorMSPE(BaseImageProcessor):
         processed_images = []
         for image in images:
             if do_resize:
-                import pdb;pdb.set_trace()
+                # import pdb;pdb.set_trace()
                 resized_height, resized_width = smart_resize(
                     height,
                     width,
