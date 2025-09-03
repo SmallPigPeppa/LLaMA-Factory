89c7df83ba59561aec78fd730b23375139d89d6f
lwz <905328134@qq.com>
2025-09-03 17:32:27 +0800

    initial

--- sft_qwen2_5_vl_3b_mspe_v2.sh
+++ sft_qwen2_5_vl_3b_mspe_v2.sh
@@ -2,4 +2,5 @@ export TOKENIZERS_PARALLELISM=false
 export WANDB_PROJECT="llama-factory"
 #ps -eo pid,comm | awk '$2~/^python(3)?$/&&$1>10000{print $1}' | xargs --no-run-if-empty kill -9
 FORCE_TORCHRUN=1 \
+CUDA_VISIBLE_DEVICES=0 \
 llamafactory-cli train examples/train_full/qwen2_5vl_3b_full_sft_mspe_v2.yaml
